ce4c9badb191ac9ff12c8f3516a29a9bcc585f53
MagicMarvMan <marvin.kai@gmx.de>
2019-10-01 12:56:39 +0200

    Add badge builder site

--- builder.js
+++ builder.js
@@ -1,11 +1,13 @@
 'use strict';
 
+const fs = require("fs")
+
 module.exports.handler = async event => {
     return {
         statusCode: 200,
         headers: {
             "Content-Type": "text/html"
         },
-        body: "<!DOCTYPE html><html><head><meta charset='utf-8'></head><body><h1>Builder - WIP</h1></body></html>"
+        body: fs.readFileSync("builder.html", "utf8")
     }
 };
