164a7667eaf4e4270aa2b0f7925b779f6ce4ecb3
MagicMarvMan <marvin.kai@gmx.de>
2019-10-01 13:20:41 +0200

    Add redirect from typo to non-typo builder page

--- builder.js
+++ builder.js
@@ -2,6 +2,16 @@
 
 const fs = require("fs")
 
+module.exports.redirect = async event => {
+    return {
+        statusCode: 301,
+        headers: {
+            "Location": "/builder",
+        },
+        body: "Redirecting to /builder..."
+    }
+}
+
 module.exports.handler = async event => {
     return {
         statusCode: 200,
